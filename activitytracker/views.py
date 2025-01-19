695b17b450b0e6b1ef5679be8c9b4f7d2326b0ec
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-01-19 18:53:52 +0000

    Added activity list view.

--- activitytracker/views.py
+++ activitytracker/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from .models import Activity
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ActivityForm
@@ -24,5 +26,17 @@ def record_activity(request):
 def activity_list(request):
     activities = request.user.activity_set.all()  # Assuming a foreign key to User
     return render(request, 'tracker/activity_list.html', {'activities': activities})
+
+def update_activity(request, pk):
+    activity = get_object_or_404(Activity, pk=pk, user=request.user)  # Ensure only the owner can edit
+    if request.method == 'POST':
+        form = ActivityForm(request.POST, instance=activity)
+        if form.is_valid():
+            form.save()
+            return redirect('activity_list')  # Redirect to the activity list page after updating
+    else:
+        form = ActivityForm(instance=activity)
+
+    return render(request, 'tracker/update_activity.html', {'form': form})  
 
 
