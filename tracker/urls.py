dc339292ebd9e8c7b31926c02285e05c6399991e
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-01-21 17:36:35 +0000

    Added html files to templates.

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from activitytracker import views as activitytracker_views
 from django.conf import settings
 from django.conf.urls.static import static
