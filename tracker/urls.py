ce8d6ae7b893db09aa7d688817ce64948f626bc6
RayPT808 <qiauxirt@students.codeinstitute.net>
2025-01-21 19:47:56 +0000

    Trying to align the two urls files.

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,12 +5,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', activitytracker_views.home, name='home'),  # Home page
+    path('', activitytracker_views.home, name='home'),
+    path('home/', activitytracker_views.home, name='home'), # Home page
     path('admin/', admin.site.urls),  # Admin panel
     path('activitytracker/record/', activitytracker_views.record_activity, name='record_activity'),  # Record activity
     path('activitytracker/list/', activitytracker_views.activity_list, name='activity_list'),  # List activities
     path('update/<int:pk>/', activitytracker_views.update_activity, name='update_activity'),  # Update activity
     path('activitytracker/', include('activitytracker.urls')),  # Include app URLs
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
