8c6e5f7e322c5d286aa91d41dbf5b65587e13564
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2025-03-11 17:26:09 +0000

    Added password matching validation.

--- activitytracker/forms.py
+++ activitytracker/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 from .models import Activity
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,12 +17,21 @@ class CustomUserCreationForm(UserCreationForm):
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
         }
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 != password2:
+            raise ValidationError("Passwords don't match.")
+        return password2
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
+    
     
 class UserProfileForm(forms.ModelForm):
     class Meta:
