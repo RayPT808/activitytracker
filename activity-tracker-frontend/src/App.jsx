85f93cddd27ddeb8bc9b764f42f5f4386867ed4c
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2025-03-29 18:17:29 +0000

    Removed RegisterPage.jsx to simplify the registration process.

--- activity-tracker-frontend/src/App.jsx
+++ activity-tracker-frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import Register from "./components/Register";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage"; // Optional 404 page
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
     );
 };
 
 export default App;
+
 
