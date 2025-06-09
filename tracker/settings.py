e48a4ad8c1662439240a1b7f3c7018926745a3be
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2025-06-09 13:07:41 +0000

    Updated url path in  dashboard.html to fix navbar issues.

--- tracker/settings.py
+++ tracker/settings.py
@@ -82,7 +82,9 @@ ROOT_URLCONF = "tracker.urls"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
