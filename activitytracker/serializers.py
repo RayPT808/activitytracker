32f1a10ba391e223736eab716f4691942581cd90
RayPT808 <159153312+RayPT808@users.noreply.github.com>
2025-06-11 16:17:08 +0000

    Changed duration in Activity Serializer again.

--- activitytracker/serializers.py
+++ activitytracker/serializers.py
@@ -32,32 +32,25 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 # ----------------------------------------
 # Activity Serializer
-# ----------------------------------------
+
 class ActivitySerializer(serializers.ModelSerializer):
-    duration = serializers.IntegerField()
+    duration = serializers.SerializerMethodField()
 
     class Meta:
         model = Activity
         fields = '__all__'
         read_only_fields = ['user']
 
-    def validate_duration(self, value):
-        if value <= 0:
-            raise serializers.ValidationError("Duration must be greater than zero.")
-        return value
+    def get_duration(self, obj):
+        return int(obj.duration.total_seconds()) if obj.duration else 0
 
     def create(self, validated_data):
-        seconds = validated_data.pop("duration", 0)
-        validated_data["duration"] = timedelta(seconds=seconds)
+        seconds = validated_data.pop('duration', 0)
+        validated_data['duration'] = timedelta(seconds=seconds)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if "duration" in validated_data:
-            seconds = validated_data.pop("duration")
-            validated_data["duration"] = timedelta(seconds=seconds)
+        if 'duration' in validated_data:
+            seconds = validated_data.pop('duration')
+            validated_data['duration'] = timedelta(seconds=seconds)
         return super().update(instance, validated_data)
-
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        ret["duration"] = int(instance.duration.total_seconds()) if instance.duration else 0
-        return ret
